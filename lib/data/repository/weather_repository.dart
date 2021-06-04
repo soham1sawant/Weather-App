11eff2b402a27a1eaa0d07c9676e958858173348
Soham Sawant <geekysoham@gmail.com>
2021-06-04 11:54:31 +0530

    created data provider and repository

---
+++ lib/data/repository/weather_repository.dart
@@ -0,0 +1,18 @@
+import 'package:weather_app/data/data_provider/weather_api.dart';
+import 'package:weather_app/data/model/weather_model.dart';
+import 'package:weather/weather.dart';
+
+class WeatherRepository {
+  WeatherApi weatherApi;
+
+  Future<WeatherModel> getWeatherForLocation() async {
+    final rawWeather = await weatherApi.getRawWeather();
+    final WeatherModel weather = WeatherModel.fromJson(rawWeather);
+    return weather;
+  }
+
+  Future<Weather> getCurrentDayWeather() async {
+    final currentWeather = await weatherApi.getCurrentWeather();
+    return currentWeather;
+  }
+}
