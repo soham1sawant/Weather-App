c9bd0c7fcea743b3ccfcd0dabda0434320493091
Soham Sawant <geekysoham@gmail.com>
2021-06-19 20:03:03 +0530

    changed home widget to stateless

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -3,12 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/logic/bloc/current_bloc/current_bloc.dart';
 import 'package:weather_app/logic/bloc/next_days_bloc/next_days_bloc.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
