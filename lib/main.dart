96bbe150a394e9c578b83c12cdf63a32d43f8a00
Soham Sawant <geekysoham@gmail.com>
2022-07-06 19:59:18 +0530

    reinitialized project

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() {
 class App extends StatelessWidget {
   final WeatherRepository weatherRepository;
 
-  const App({Key key, @required this.weatherRepository}) : super(key: key);
+  const App({Key key, required this.weatherRepository}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
