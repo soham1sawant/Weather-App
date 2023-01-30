3ddfaae2839e63c013cd0ac166f0bdda76638612
Soham Sawant <geekysoham@gmail.com>
2023-01-30 19:29:15 +0530

    started with layered arch

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'data/repository/weather_repository.dart';
-import 'logic/bloc/current_bloc/current_bloc.dart';
-import 'logic/bloc/next_days_bloc/next_days_bloc.dart';
-import 'logic/simple_bloc_observer.dart';
-import 'presentation/screens/home.dart';
 
 void main() {
-
-  final WeatherRepository weatherRepository = WeatherRepository();
-
-  BlocOverrides.runZoned(
-    () => runApp(App(weatherRepository: weatherRepository)),
-    blocObserver: SimpleBlocObserver(),
-  );
+  runApp(const MainApp());
 }
 
-class App extends StatelessWidget {
-  final WeatherRepository weatherRepository;
-
-  const App({Key? key, required this.weatherRepository}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CurrentBloc>(
-            create: (context) =>
-                CurrentBloc(weatherRepository)..add(CurrentRequested())),
-        BlocProvider<NextDaysBloc>(
-            create: (context) =>
-                NextDaysBloc(weatherRepository)..add(NextDaysRequested())),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Home(),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
